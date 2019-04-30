66dfc214801a874fbcbf4f4335bc46bb57258d53
wensug <wensug@gmail.com>
2019-04-30 11:51:18 +0100

    Updates

---
+++ precache-manifest.4545476052f2e4d6ecc2b53d95f6cf64.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "331d7beb524fd1876f989096beee99c7",
+    "url": "/portfolio/index.html"
+  },
+  {
+    "revision": "eb380395c601dc148f83",
+    "url": "/portfolio/static/css/main.584f321a.chunk.css"
+  },
+  {
+    "revision": "2e0129063922e3bb197a",
+    "url": "/portfolio/static/js/2.b41502e9.chunk.js"
+  },
+  {
+    "revision": "eb380395c601dc148f83",
+    "url": "/portfolio/static/js/main.510691a5.chunk.js"
+  },
+  {
+    "revision": "53ebc025fca807d4dbea",
+    "url": "/portfolio/static/js/runtime~main.f308c8e6.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/portfolio/static/media/logo.5d5d9eef.svg"
+  }
+]);
