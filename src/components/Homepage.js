d3d3d28958c13e196240e4da1424bdc6d2f15a43
wensug <wensug@gmail.com>
2019-05-18 23:25:42 +0100

    responsive and style

--- src/components/Homepage.js
+++ src/components/Homepage.js
@@ -1,70 +1,152 @@
-import React, { Component } from 'react';
-import './Homepage.css';
-import Projects from './Projects';
-import About from './About';
-import Contact from './Contact';
+import React, { Component } from "react";
+import { NavLink } from "react-router-dom";
+import "./Header/Header.css";
+import "./Homepage.css";
+import Projects from "./Projects";
+import About from "./About";
+import Contact from "./Contact";
 
+function scrollElement(id) {
+  console.log(id);
+  document.querySelector(id).scrollIntoView({
+    behavior: "smooth"
+  });
+}
 
 class Homepage extends Component {
-
-    render() {
-        return (
-            <div class="container" id="home">
-                <div className="section hero">
-                    <div className="row">
-                        <div className="description">
-                            <h1>I'm Wendy a Frontend Developer</h1>
-                            <h2>I'm Wendy a Frontend Developer</h2>
-                        </div>
-                    </div>
+  render() {
+    return (
+      <div class="container" id="home">
+        <div className="section hero">
+          <div className="row">
+            <div>
+              <h1>I'm Wendy a Frontend Developer</h1>
+              <h2>I'm Wendy a Frontend Developer</h2>
+              <p className="tags">
+                <div>
+                  <span className="tag">
+                    <i className="fab fa-js-square" />
+                    <b>JavaScript</b>
+                  </span>
                 </div>
-
-                <div className="section projects">
-                    <div className="row">
-                        <h3 className="row-title">Projects</h3>
-                        <div class="card-container">
-                            <div className="card">
-                                <div className="card-image">
-                                    <img src="http://placekitten.com/200/300" />
-                                </div>
-                                <div className="card-text">
-                                    <h4>I'm Wendy a Frontend Developer</h4>
-                                    <p>ghfhgfhgfh</p>
-                                </div>
-                            </div>
-                            <div className="card">
-                                <div className="card-image">
-                                    <img src="http://placekitten.com/200/300" />
-                                </div>
-                                <div className="card-text">
-                                    <h4>I'm Wendy a Frontend Developer</h4>
-                                    <p>ghfhgfhgfh</p>
-                                </div>
-                            </div>
-                            <div className="card">
-                                <div className="card-image">
-                                    <img src="http://placekitten.com/200/300" />
-                                </div>
-                                <div className="card-text">
-                                    <h4>I'm Wendy a Frontend Developer</h4>
-                                    <p>ghfhgfhgfh</p>
-                                </div>
-                            </div>
-                        </div>
-                    </div>
+                <div>
+                  <span className="tag">
+                    <i className="fab fa-react" />
+                    <b>React</b>
+                  </span>
                 </div>
-
-                <div className="section contact">
-                    <div className="row">
-                        <div className="description">
-                            <h3>Contact</h3>
-                            <a href="#" class="button">Contact</a>
-                        </div>
-                    </div>
+                <div>
+                  <span className="tag">
+                    <i className="fab fa-node" />
+                    <b>Node js</b>
+                  </span>
                 </div>
-
+              </p>
+              <p className="tags">
+                <div>
+                  <span className="tag">
+                    <i className="fab fa-html5" />
+                    <b>HTML5</b>
+                  </span>
+                </div>
+                <div>
+                  <span className="tag">
+                    <i className="fab fa-css3-alt" />
+                    <b>CCS3</b>
+                  </span>
+                </div>
+                <div>
+                  <span className="tag">
+                    <i className="fas fa-database" />
+                    <b>MONGODB</b>
+                  </span>
+                </div>
+                <div>
+                  <span className="tag">
+                    <i className="fab fa-python" />
+                    <b>Python</b>
+                  </span>
+                </div>
+              </p>
             </div>
-        );
-    }
+          </div>
+        </div>
+
+        <div className="section projects">
+          <div className="row">
+            <h3 className="row-title">Projects</h3>
+            <div class="card-container">
+              <div className="card">
+                <div className="card-image">
+                  <img src="http://placekitten.com/200/300" />
+                </div>
+                <div className="card-text">
+                  <h4>I'm Wendy a Frontend Developer</h4>
+                  <p>ghfhgfhgfh</p>
+                </div>
+              </div>
+              <div className="card">
+                <div className="card-image">
+                  <img src="http://placekitten.com/200/300" />
+                </div>
+                <div className="card-text">
+                  <h4>I'm Wendy a Frontend Developer</h4>
+                  <p>ghfhgfhgfh</p>
+                </div>
+              </div>
+              <div className="card">
+                <div className="card-image">
+                  <img src="http://placekitten.com/200/300" />
+                </div>
+                <div className="card-text">
+                  <h4>I'm Wendy a Frontend Developer</h4>
+                  <p>ghfhgfhgfh</p>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+
+        <div className="section contact">
+          <div className="row">
+            <div className="description">
+              <h3>Contact</h3>
+              <a href="#" class="button">
+                Send me an email
+              </a>
+              <div className="nav">
+                <ul className="menu">
+                  <li>
+                    <NavLink
+                      onClick={() => scrollElement("#projects")}
+                      to="/#projects"
+                    >
+                      <i className="fab fa-github" />
+                    </NavLink>
+                  </li>
+                  <li>
+                    <NavLink
+                      onClick={() => scrollElement("#projects")}
+                      to="/#projects"
+                    >
+                      <i className="fab fa-linkedin" />
+                    </NavLink>
+                  </li>
+                  <li>
+                    <NavLink
+                      onClick={() => scrollElement("#projects")}
+                      to="/#projects"
+                    >
+                      <i className="fab fa-twitter" />
+                    </NavLink>
+                  </li>
+                </ul>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    );
+  }
 }
 export default Homepage;
--- src/components/header/Header.js
+++ src/components/header/Header.js
@@ -1,32 +1,51 @@
-import React from 'react';
-import { NavLink } from 'react-router-dom';
-import './Header.css';
+import React from "react";
+import { NavLink } from "react-router-dom";
+import "./Header.css";
 
 function scrollElement(id) {
-  console.log(id)
-  document.querySelector(id).scrollIntoView ( {
-  behavior: 'smooth'
- });
+  console.log(id);
+  document.querySelector(id).scrollIntoView({
+    behavior: "smooth"
+  });
 }
 
 export default function Header() {
   return (
     <header>
       <div className="nav">
-        <p className="logo">Wensug</p>
+        <p className="logo" onClick={() => scrollElement("#home")} to="/#home">Wensug</p>
         <ul className="menu">
-          <li><NavLink onClick={() => scrollElement('#home')} to='/#home'>Home</NavLink></li>
-          <li><NavLink onClick={() => scrollElement('#about')} to='/#about'>About</NavLink></li>
-          <li><NavLink onClick={() => scrollElement('#projects')} to='/#projects'>Projects</NavLink></li>
-          <li><NavLink onClick={() => scrollElement('#contact')} to='/#contact'>Contact</NavLink></li>
+          <li>
+            <NavLink onClick={() => scrollElement("#about")} to="/#about">
+              About
+            </NavLink>
+          </li>
+          <li>
+            <NavLink onClick={() => scrollElement("#projects")} to="/#projects">
+              Projects
+            </NavLink>
+          </li>
+          <li>
+            <NavLink onClick={() => scrollElement("#projects")} to="/#projects">
+            <i className="fab fa-github"></i>
+            </NavLink>
+          </li>
+          <li>
+            <NavLink onClick={() => scrollElement("#projects")} to="/#projects">
+            <i className="fab fa-linkedin"></i>
+            </NavLink>
+          </li>
+          <li>
+            <NavLink
+              className="button-xs"
+              onClick={() => scrollElement("#contact")}
+              to="/#contact"
+            >
+              Contact
+            </NavLink>
+          </li>
         </ul>
       </div>
     </header>
   );
 }
-
-    {/* <div className='footer-media'>
-            <a href='https://github.com/wensug'><i className="fab fa-github"></i></a>
-            <a href='https://www.linkedin.com/in/wendygonzalezcriollo/'><i className="fab fa-linkedin"></i></a>
-            <a href='https://twitter.com/wendygoncriollo'><i className="fab fa-twitter"></i></a>
-            </div> */}
